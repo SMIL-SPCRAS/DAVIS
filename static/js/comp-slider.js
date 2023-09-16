6e2ae8d51047ad63b3e176a6dda35bc7f8dd0147
Alex <36799721+MiSTeR1995@users.noreply.github.com>
2023-09-16 14:53:08 +0300

    added DB, tables, arch, results

--- static/js/comp-slider.js
+++ static/js/comp-slider.js
@@ -1,5 +1,14 @@
-const container = document.querySelector('.comp-container');
+const slider_first = document.querySelector('.slider-first');
+const slider_second = document.querySelector('.slider-second');
+const slider_third = document.querySelector('.slider-third');
 
-document.querySelector('.slider').addEventListener('input', (e) => {
-    container.style.setProperty('--position', `${e.target.value}%`);
+document.querySelector('.slider-input-first').addEventListener('input', (e) => {
+    slider_first.style.setProperty('--position', `${e.target.value}%`);
+});
+
+document.querySelector('.slider-input-second').addEventListener('input', (e) => {
+    slider_second.style.setProperty('--position', `${e.target.value}%`);
+});
+document.querySelector('.slider-input-third').addEventListener('input', (e) => {
+    slider_third.style.setProperty('--position', `${e.target.value}%`);
 });
